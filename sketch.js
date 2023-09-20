7707b789b95f8f16d678ba050c30facd398baa68
tshorizons <123700523+tshorizons@users.noreply.github.com>
2023-09-20 17:30:01 +0100

    Second Commit

--- sketch.js
+++ sketch.js
@@ -38,4 +38,35 @@ function draw() {
   fill(300,128,64,40);
   rect(200,200,100,100);
 
+  //Bottom Pain
+  //Top Rectangles - Base
+  translate(0,400);
+  fill(220,64,128,50);
+  rect(0,0,200,200);
+
+  fill(120,128,64,50);
+  rect(200,0,200,200);
+
+  //Bottom Rectangles - Base
+
+  fill(120,64,128,50);
+  rect(0,200,200,200);
+
+  fill(220,128,64,40);
+  rect(200,200,200,200);
+
+  //Cut out Rectangles - Top
+  fill(300,128,128,50);
+  rect(100,100,100,100);
+
+  fill(300,128,64,50);
+  rect(200,100,100,100);
+
+  //Cut out Rectangles - Base
+  fill(300,64,128,50);
+  rect(100,200,100,100);
+
+  fill(40,128,64,40);
+  rect(200,200,100,100);
+
 }
